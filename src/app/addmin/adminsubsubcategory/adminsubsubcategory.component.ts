0cb5e8de5eba467081fa6b834ef4a671f9e29e50
pawansinghparmar <pawansinghparmar08@gmail.com>
2019-11-26 11:25:01 +0530

    2611commit

--- src/app/addmin/adminsubsubcategory/adminsubsubcategory.component.ts
+++ src/app/addmin/adminsubsubcategory/adminsubsubcategory.component.ts
@@ -18,10 +18,11 @@ public subfields={text:'name',value:'_id'};
 public placeholder="please select any category";
 public subplaceholder="please select any Subcategory"
   constructor(private http:HttpClient) { }
-  foods;Catdata;selectedcategoryid;
+  foods;Catdata;selectedcategoryid;SubCatData;
 
   ngOnInit() {
     this.getAllCategories();
+    this.getAllsubCategories();
     this.getAllSubSubCategoryData();
     // this.foods=[ {value: 'steak-0', viewValue: 'Steak'},
     // {value: 'pizza-1', viewValue: 'Pizza'},
@@ -32,15 +33,23 @@ public subplaceholder="please select any Subcategory"
   }
   getAllCat=(ct)=>{
     this.Catdata = ct;
-    
-    // this.FilterSubCategory();
   } 
+
+  getAllsubCategories(){
+    this.http.get('/api/subcategory/getAllSubCategories').subscribe(this.getAllSubCat)
+  }
+  getAllSubCat=(subct)=>{
+      this.SubCatData=subct;
+     
+  } 
+
+
   subcategoryData;
-filterCategory = [];
-// subcategoryData
- check(event){
-  this.filterCategory = [];
-  this.Catdata.forEach(element => {
+  filterCategory = [];
+  // subcategoryData
+  check(event){
+   this.filterCategory = [];
+   this.Catdata.forEach(element => {
     if(element['_id'] == event.value){
       element['getcategorySubcategory'].forEach(ele => {
         if(element['_id'] == event.value){
@@ -51,6 +60,7 @@ filterCategory = [];
   });
   
   this.subcategoryData=this.filterCategory;
+  console.log(this.subcategoryData);
   
  }
 
@@ -84,4 +94,79 @@ filterCategory = [];
    console.log(res)
    this.SubSubCategoryData=res;
  }
-}
+ deleteSubSubCategory(id){
+  console.log(id);
+  this.http.post('/api/subsubcategory/deletesubsubcategorybyid',{id:id}).subscribe(this.deletesubcat)
+  }
+  deletesubcat=(res)=>{
+    this.getAllSubSubCategoryData();
+  }
+  
+  editsubcat;subsubname;subsubdes;subname;catname;creationdate;catid;subcatid;subsubcatid;
+  result;subsubcatname;
+  editSubSubCategory(id){
+    this.http.post('/api/subsubcategory/getsubsubcategorybyid',id).subscribe(this.editsubcat)
+   
+   this.editsubcat =(res)=>{
+    this.result=res;
+    console.log(this.result);
+    this.catname= this.result.getsCategory;
+    this.subsubcatid=this.result.id;
+    this.subsubcatname=this.result.name;
+    this.subsubdes = this.result.description;
+    console.log(this.result.id,this.catname);
+    } 
+      
+    this.openform()
+  }
+    
+    SubSubCategoryEditForm=new FormGroup({
+     id:new FormControl(''),
+      categoryid:new FormControl(''),
+      subcategoryid:new FormControl(''),
+      name: new FormControl(''),
+      description:new FormControl(''),
+      
+     });
+   
+    
+   
+  updateSubSubCategory(sscobj){
+  
+    this.SubSubCategoryEditForm.setValue({id:this.subsubcatid,
+      categoryid:sscobj.categoryid,
+      subcategoryid:sscobj.subcategoryid,
+      name:sscobj.name,description:sscobj.description});
+   console.log(this.SubSubCategoryEditForm.value);
+  
+    
+   
+     this.http.put('/api/subsubcategory/editsubsubcategory',this.SubSubCategoryEditForm.value).subscribe(r=>{
+    
+     this.getAllSubSubCategoryData();
+   })
+  }
+  
+
+
+
+
+  cancleSubSubCategory(){
+    this.editsubcat = 1;
+   }
+
+
+
+
+
+ openform(){
+   document.getElementById("showdata").style.display="none";
+   document.getElementById("showform").style.display="block";
+   
+ }
+ showdata(){
+  document.getElementById("showdata").style.display="block";
+  document.getElementById("showform").style.display="none";
+  
+  }
+  }
